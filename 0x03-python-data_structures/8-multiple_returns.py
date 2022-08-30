c9964838e40e09ff5f6197ef3e973d3476dca2ec
tsehayget <tsehayget4@gmail.com>
2022-08-29 20:39:59 -0700

    indentation error fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-     strlen = len(sentence)
-     if strlen == 0:
-         return strlen, None
-     firstchar = sentence[0]
-     return strlen, firstchar
+    strlen = len(sentence)
+    if strlen == 0:
+        return strlen, None
+    firstchar = sentence[0]
+    return strlen, firstchar
