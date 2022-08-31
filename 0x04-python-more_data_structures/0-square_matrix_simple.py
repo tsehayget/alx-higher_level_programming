3eb59c7c537d69b746006a19ba2b187f9c652080
tsehayget <tsehayget4@gmail.com>
2022-08-31 15:11:43 -0700

    error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,5 @@ def square_matrix_simple(matrix=[]):
     index = 0
 
     for elements in matrix:
-         new_matrix.append(list(map(lambda elements: elements ** 2, elements)))
+        new_matrix.append(list(map(lambda elements: elements ** 2, elements)))
     return new_matrix
