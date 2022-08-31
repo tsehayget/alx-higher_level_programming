622792677592d7f31e035c586317f35159b84fa1
tsehayget <tsehayget4@gmail.com>
2022-08-31 14:33:27 -0700

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    import sys
+from calculator_1 import add, mul, sub, div
+from sys import argv
 
-    if len(sys.argv) - 1 != 3:
+if __name__ == '__main__':
+    argc = (len(argv) - 1)
+    operators = ['+', '-', '*', '/']
+    functions = [add, sub, mul, div]
+
+    if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        a = int(argv[1])
+        b = int(argv[3])
 
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+        for symbols in range(len(operators)):
+            if argv[2] == operators[symbols]:
+                calc = functions[symbols](a, b)
+                sign = operators[symbols]
+                break
+
+        if argv[2] != operators[symbols]:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+
+            print("{:d} {} {:d} = {:d}".format(a, sign, b, calc))
+            exit(0)
